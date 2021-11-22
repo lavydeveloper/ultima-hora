3a7c1196d991f8efd182b7a080f7ba4dcd3ececd
lavydeveloper <lavy.prof@gmail.com>
2021-11-22 14:48:00 +0100

    Maquetando cabecera no responsive

---
+++ js/match-media.js
@@ -0,0 +1,6 @@
+/*function myFunction() {
+    var element = document.querySelector(".top-box__element");
+    if (window.matchMedia("(max-width: 992px)").matches) {
+        element.classList.add("hide");
+    }
+  }*/
