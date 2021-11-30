837669597521df7dd0cfb806d75785d18da16c77
lavydeveloper <lavy.prof@gmail.com>
2021-11-30 15:01:11 +0100

    Progress-bar Javascript

--- js/movement.js
+++ js/movement.js
@@ -11,9 +11,9 @@ window.addEventListener("scroll", () => {
 
     let winScroll = document.documentElement.scrollTop;
     let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-
-    console.log(height);
+    console.log(height)
     let scrolled = (winScroll / height) * 100;
+    let stickyMenu = (winScroll / height) * 100;
   
     document.getElementById("progressBar").style.width = scrolled + "%";
 });
