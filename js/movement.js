c90176f9017f37a3969fa6172dadd0aa33f0c99b
lavydeveloper <lavy.prof@gmail.com>
2021-11-30 13:24:37 +0100

    JS progress bar

--- js/movement.js
+++ js/movement.js
@@ -6,3 +6,14 @@ openNav.addEventListener("click", () => {
     navList.classList.toggle("active");
     opacity.classList.toggle("active");
 });
+
+window.addEventListener("scroll", () => {
+
+    let winScroll = document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+    console.log(height);
+    let scrolled = (winScroll / height) * 100;
+  
+    document.getElementById("progressBar").style.width = scrolled + "%";
+});
